784b1b774e6389e9ba57cc5bf3dcf75a2811acbe
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2023-02-04 01:57:40 +0900

    화살 테스트

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24f604861812f4411373f01408ab1c50c3c810e56c11bf1214df14d272f7f3d3
-size 839
+oid sha256:90c5f4c7cb257d19d14fc41e35e22acfb50372b28eda1cd6a9699bccd3eba101
+size 897
