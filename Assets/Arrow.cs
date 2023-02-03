4276d9cbc654f735a15c7a2eabd2fe418a58960e
ZZalbs <57441250+ZZalbs@users.noreply.github.com>
2023-02-04 01:12:08 +0900

    화살 클릭 시 움직임 구현

--- Assets/Arrow.cs
+++ Assets/Arrow.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:deffc78e7f875d164aa8df0e66f657db516840a5c0909465aa8233b036c09326
-size 445
+oid sha256:24f604861812f4411373f01408ab1c50c3c810e56c11bf1214df14d272f7f3d3
+size 839
